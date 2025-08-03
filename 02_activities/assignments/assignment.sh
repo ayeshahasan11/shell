54645b039ad28c698f6d0a73a6c9dfc0aa761b2f
Ayesha Hasan <ayeshahasan11@gmail.com>
2025-08-03 13:52:25 -0400

    Created inventory.txt file in data/ and assigned list of all files in subfolders of data/processed/

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -54,6 +54,7 @@ cp data/raw/*event*.log data/processed/event_logs/
 rm data/raw/*ipaddr* data/processed/user_logs/*ipaddr*
 
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
+ls -r data/processed/user_logs/ data/processed/server_logs/ data/processed/event_logs/ > data/inventory.txt
 
 
 ###########################################
